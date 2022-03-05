ef750d6224deb7c7dcab44592027229f19c81859
MHojimurod <cool.hojimurod2001@mail.ru>
2022-03-05 18:22:57 +0500

    bat file push

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('contact/',contact,name="contact"),
+    path('about/',about,name="about"),
+    path('news/',news,name="news"),
+    path('works/',works,name="works"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+def news(request):
+    return render(request, 'main/news-grid.html')
+def works(request):
+    return render(request, 'main/works-grid.html')
